930b90331be853cdbc8bccadbfc7f888e81d5874
temaEv <114759588+temaEv@users.noreply.github.com>
2022-11-07 15:16:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Евтушенко А.И. </title>
+<link rel="stylesheet" href="mycss.css">
+<link rel="shortcut icon" href="favicon.iso">
+</head>
+<body>
+<div class="mydiv1">
+<?php
+echo "Периметр треугольника со сторонами 8, 7 и 6 равен ";
+echo 8+7+6;
+?>
+</div>
+</body>
+</html>
